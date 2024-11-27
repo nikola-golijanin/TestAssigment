876f63edabcb69d5b89f911de41ad4a23c8d379c
Nikola Golijanin <nikola.golijanin@dinitcs.com>
2024-11-27 13:06:09 +0100

    Add NuGet configuration and update application settings; refine WeatherForecastController

--- TestAssigmentAPI/Controllers/WeatherForecastController.cs
+++ TestAssigmentAPI/Controllers/WeatherForecastController.cs
@@ -8,6 +8,7 @@ namespace TestAssigmentAPI.Controllers
     [ServiceFilter(typeof(ApiKeyAttribute))]
     public class WeatherForecastController : ControllerBase
     {
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
